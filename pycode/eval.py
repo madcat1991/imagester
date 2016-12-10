0950674e8bc5138bde3ef2ee6960bc193c963658
Tural Gurbanov <tural.gurbanov@inf.unibz.it>
2016-12-10 17:18:05 +0100

    Updated eval

--- pycode/eval.py
+++ pycode/eval.py
@@ -1,8 +1,10 @@
 # coding: utf-8
 from cache import AppCacheRedis
-from etc.local.config import CLARIFAI_APP_ID, CLARIFAI_APP_SECRET
+from etc.local.config import CLARIFAI_APP_ID, CLARIFAI_APP_SECRET, HASHTAG_URL_TEMPLATE
 from img_to_kw import ClarifAI
 from redis import Redis
+
+from kw_to_hashtag import HashtagMiner, HashtagDj
 
 if __name__ == '__main__':
     cache = AppCacheRedis(Redis())
@@ -13,8 +15,10 @@ if __name__ == '__main__':
     if kws is None:
         kws = im_app.get_keywords(image_path)
         cache.set(image_path, kws)
-        print "NOT"
-    else:
-        print "HIT"
 
-    print kws
+    miner = HashtagMiner(HASHTAG_URL_TEMPLATE)
+    dj = HashtagDj(miner, cache)
+
+    result = dj.get_hashtags(kws)
+    print result["extended"]
+    print dj.to_instagram(result["tags"])
