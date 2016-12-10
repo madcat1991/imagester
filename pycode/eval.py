d6bfa8a531ce60492f62b60f615cf9309de2ef8f
Tural Gurbanov <tural.gurbanov@inf.unibz.it>
2016-12-10 17:41:45 +0100

    Work with flask config

--- pycode/eval.py
+++ pycode/eval.py
@@ -1,14 +1,18 @@
 # coding: utf-8
+from flask import Config
+
 from cache import AppCacheRedis
-from etc.local.config import CLARIFAI_APP_ID, CLARIFAI_APP_SECRET, HASHTAG_URL_TEMPLATE
 from img_to_kw import ClarifAI
 from redis import Redis
 
 from kw_to_hashtag import HashtagMiner, HashtagDj
 
 if __name__ == '__main__':
+    config = Config('')
+    config.from_pyfile("etc/local/config.py")
+
     cache = AppCacheRedis(Redis())
-    im_app = ClarifAI(CLARIFAI_APP_ID, CLARIFAI_APP_SECRET)
+    im_app = ClarifAI(config["CLARIFAI_APP_ID"], config["CLARIFAI_APP_SECRET"])
 
     image_path = "/Users/user/Downloads/ialmeida-9d0005ddf4b69c67820a0143f995a3a9-1.jpg"
     kws = cache.get(image_path)
@@ -16,7 +20,7 @@ if __name__ == '__main__':
         kws = im_app.get_keywords(image_path)
         cache.set(image_path, kws)
 
-    miner = HashtagMiner(HASHTAG_URL_TEMPLATE)
+    miner = HashtagMiner(config["HASHTAG_URL_TEMPLATE"])
     dj = HashtagDj(miner, cache)
 
     result = dj.get_hashtags(kws)
